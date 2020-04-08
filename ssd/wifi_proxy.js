b7645ec091583adee540ca6d7458eda69d829080
xiaoming123m <49673642+xiaoming123m@users.noreply.github.com>
2020-04-08 12:34:10 +0800

    Add files via upload

--- ssd/wifi_proxy.js
+++ ssd/wifi_proxy.js
@@ -2,15 +2,18 @@
 文本编辑模式下复制粘贴,TG频道已发操作教程视频
 event network-changed debug=1,script-path=wifi_proxy.js
 PS:记得自己修改WIFI名称
+主要功能:指定Wi-Fi(路由器翻)下,Surge使用直连模式,其他网络下Surge使用规则模式
 虽然设置SSID可以达到基本相同功能
 使用脚本,Surge不会被suspend
 Rewrite和Scripting依然有效
-https://meetagit.github.io/MeetaRules/Surge/Scripting/wifi_proxy.js
 TG频道:@meetashare
 */
 
+
+
 var wifiname = $network.wifi.ssid;
-var proxywifi = "大喊baba即可科学上网";
+var proxywifi = "ASUS";
+var proxywifi = "citic_wlan";
 if (wifiname == proxywifi){
 	$surge.setOutboundMode("direct");
 	//$notification.post("Meeta_Remind","您目前处于WIFI-Proxy","Surge已自动变为直连模式");
@@ -21,4 +24,3 @@ else{
 	//$notification.post("Meeta_Remind","Surge已自动变为规则模式","");
 }
 $done();
-
